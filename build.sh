3b8092ed544c841ff54f31a584070878417304bf
Garrett Hansen <garretthansen2000@gmail.com>
2021-02-15 22:24:25 -0700

    Updating build script with Django install

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-#TODO: Run build/lint setup 
+#TODO: Run build/lint setup
 
-echo "Starting up application"
+echo "Installing Dependencies"
+python -m pip install --upgrade pip
+if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
+python -m pip install Django
+
+echo "Running Django Migrations"
+python soil/manage.py migrate
+
+echo "Starting up the application"
 python soil/manage.py runserver
+
 exit 0
--- soil/manage.py
+++ soil/manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-from soil import tests
+import tests
 
 
 def main():
