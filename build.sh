67154655be216ed9712678c1a1f7d6333eedda82
Garrett Hansen <garretthansen2000@gmail.com>
2021-02-15 22:11:23 -0700

    Update build/test/clean shell scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+#TODO: Run build/lint setup 
+
+echo "Starting up application"
+python soil/manage.py runserver
 exit 0
--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+
+# TODO: Run tests here
+
 exit 0
