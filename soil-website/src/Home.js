7b5ff3a5f79d6379256f8d64aa9253a3c89aefab
Garrett Hansen <garretthansen2000@gmail.com>
2021-02-23 10:37:59 -0700

    Add Google-Auth button

--- soil-website/src/Home.js
+++ soil-website/src/Home.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import { GoogleLogin } from "react-google-login";
 
 class Home extends Component {
+
+    responseGoogle = (response) => {
+        console.log(response);
+    }
+
   render() {
     return (
       <div>
-        <h2>Home/Login Page</h2>
+            <h2>Home/Login Page</h2>
+            <GoogleLogin buttonText="Login" onSuccess={this.responseGoogle} onFailure={this.response} />
         <p>Example login page, Google login to be added here. Users will only be able to see this page unless they are logged in, then they can access the form page.</p>
       </div>
     );
