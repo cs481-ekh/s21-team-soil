2334cb5fcf15da6b9ee977c3b3cd71d310181b9c
brandoncontreras <brandoncontreras@u.boisestate.edu>
2021-02-23 11:33:09 -0700

    Fixed pylint space issue

--- src/soil/settings.py
+++ src/soil/settings.py
@@ -77,9 +77,7 @@ DATABASES = {
   #  'default': {
    #     'ENGINE': 'django.db.backends.sqlite3',
     #    'NAME': BASE_DIR / 'db.sqlite3',
-    #} 
-
-
+    #}
     # Use this connection if you wish to only use/test on your local MySQL server instance.
     #'default': {
     #    'ENGINE': 'django.db.backends.mysql',
