9b8c2ffd8371c34cb8e30de74d9ad124f5c2f6a1
Brian Morck <brianmorck@u.boisestate.edu>
2021-03-01 13:45:40 -0700

    adding migration files

--- src/Soils/migrations/0003_auto_20210228_2339.py
+++ src/Soils/migrations/0003_auto_20210228_2339.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('Soils', '0002_soilmakeup'),
+        ('Soils', '0002_soil'),
     ]
 
     operations = [
