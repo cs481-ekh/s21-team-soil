995d55740657e9cf616c0b11399f4b227c4aeec5
Brian Morck <brianmorck@u.boisestate.edu>
2021-03-02 11:29:18 -0700

    Added dependenices for api endpoint to build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ python -m pip install --upgrade pylint || { echo "Pylint install failed"; exit 1
 if [ -f requirements.txt ]; then pip install -r requirements.txt || { echo "Virtual environment requirements install failed"; exit 1; } fi
 python -m pip install --upgrade Django || { echo "Django install failed"; exit 1; }
 python -m pip install --upgrade mysqlclient || { echo "Mysql client install failed"; exit 1; }
+python -m pip install --upgrade djangorestframework || { echo "REST framework install failed"; exit 1; }
+python -m pip install --upgrade django-cors-headers || { echo "CORS app install failed"; exit 1; }
 
 echo "Running PyLint"
 pylint -d C0415 src || { echo "pylint failed"; exit 1; }
