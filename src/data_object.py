a138060aceb0a309766a704be81f838235ee3a8a
Brian Morck <brianmorck@u.boisestate.edu>
2021-03-09 10:40:11 -0700

    adding data_object.py

---
+++ src/data_object.py
@@ -0,0 +1,101 @@
+
+dataFile = []
+liquidLimit = 0
+plasticIndex = 0
+clayPercent = 0
+siltPercent = 0
+sandPercent = 0
+organicContent = 0
+limeCementStabilize = false
+limeCementDose = false
+quantResult = false
+qualResult = false
+
+def setDataFile(data):
+    dataFile = data
+    return dataFile
+
+def getDataFile():
+
+    return dataFile
+
+
+def setLiquidLimit(ll):
+    liquidLimit = ll
+    return liquidLimit
+
+def getLiquidLimit():
+
+    return liquidLimit
+
+
+def setPlasticIndex(pl):
+    plasticIndex = pl
+    return plasticIndex
+
+def getPlasticIndex():
+
+    return plasticIndex
+
+
+def setClayPercent(cp):
+    clayPercent = cp
+    return clayPercent
+
+def getClayPercent():
+
+    return clayPercent
+
+
+def setSandPercent(sp):
+    sandPercent = sp
+    return sandPercent
+
+def getSandPercent():
+
+    return sandPercent
+
+
+def setOrganicContent(oc):
+    organicContent = oc
+    return organicContent
+
+def getOrganicContent():
+
+    return organicContent
+
+
+def setLimeCementStabilize(lcs):
+    limeCementStabilize = lcs
+    return limeCementStabilize
+
+def getLimeCementStabilize():
+
+    return limeCementStabilize
+
+
+def setLimeCementDose(lcd):
+    limeCementDose = lcd
+    return limeCementDose
+
+def getLimeCementDose():
+
+    return limeCementDose
+
+
+def setQuantResult(quantr):
+    quantResult = quantr
+    return quantResult
+
+def getQuantResult():
+
+    return quantResult
+
+
+def setQualResult(qualr):
+    qualResult = qualr
+    return qualResult
+
+def getQualResult():
+
+    return qualResult
