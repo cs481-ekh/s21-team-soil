fc72228f513189e08795bcf69031b51aad427552
Garrett Hansen <garretthansen2000@gmail.com>
2021-03-18 09:35:45 -0600

    Add admin page template

--- soil-website/src/Main.js
+++ soil-website/src/Main.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import Home from "./Home";
 import Form from "./Form";
+import Admin from "./Admin";
 
 class Main extends Component {
   render() {
@@ -16,10 +17,12 @@ class Main extends Component {
                   <ul className="header">
                     <li><NavLink to="/">Home</NavLink></li>
                     <li><NavLink to="/form">Form</NavLink></li>
+                    <li><NavLink to="/admin">Admin</NavLink></li>
                   </ul>
                   <div className="content">
                     <Route exact path="/" component={Home}/>
-                    <Route path="/form" component={Form}/>
+                    <Route path="/form" component={Form} />
+                    <Route path="/admin" component={Admin} />
                   </div>
                 </div>
         </HashRouter>
