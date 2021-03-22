1d4048ea9ba5209c5512d5e02a8e61a88a33516f
brandoncontreras <brandoncontreras@u.boisestate.edu>
2021-03-22 12:55:55 -0600

    Added unit tests for user tracking. Fixed MYSQL warning. Added documentation. Added 2 ignores for pyling

--- src/soil/settings.py
+++ src/soil/settings.py
@@ -84,7 +84,8 @@ DATABASES = {
         'HOST': 'soil-dev-aws.cm1fekzgqprt.us-east-2.rds.amazonaws.com',
         'PORT': '3306',
         'OPTIONS' : {
-            'charset': 'utf8mb4'
+            'charset': 'utf8mb4',
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
         }
     }
 }
--- src/user_tracking/views.py
+++ src/user_tracking/views.py
@@ -1,30 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import User, Session
-# import datetime 
 from datetime import datetime
+
+
 def index(request):
     return HttpResponse("Viewing user_tracking index. Placeholder.")
 
 def log_session(user_google_id, user_name, user_email):
-    """[summary]
+    """Creates a new session in database for provided user. If user is a new unseen user, adds
+    a new user to database as well..
 
     Args:
-        user_google_id ([type]): [description]
-        user_name ([type]): [description]
-        user_email ([type]): [description]
+        user_google_id (int): google id of user, provided by google authenticate
+        user_name (str): Full name of user
+        user_email (str): Email of user
 
     Returns:
-        [type]: [description]
+        None: Stores in database with no return values
     """
 
-    # WORKS
-
-    # Look up user, 
-    # if existing get primary ID. 
-    # if new, create new user
-    # then, create new session model no matter what that depends on the user retrieved/created
-    
     # See if user exists in database. Query by their google_id.
     try:
         user = User.objects.get(google_id=user_google_id)
@@ -46,28 +41,37 @@ def log_session(user_google_id, user_name, user_email):
     return None
 
 def all_users():
-    #  WORKS
+    """Returns all users in database sorted by name
+
+    Returns:
+        QuerySet[User]: List containing each User instance
+    """
     # Return all unique users in Database. 
     # sorted by full name
     # returns query set. same as python list. Each index in user_list is a user model.
     user_list = User.objects.order_by("full_name")
-
-    # TODO: take out
-    for user in user_list:
-        print(user)
-
     return user_list    
 
 def all_sessions():
+    """Returns all sessions in database sorted by time.
+
+    Returns:
+        QuerySet[Session]: List containing each Session instance
+    """
     session_list = Session.objects.order_by("log_time")
-    
-    # TODO: Take out
-    for s in session_list:
-        print(s)
+
     return session_list
 
-def get_sessions_from_user(user_pk):
-    # Where user_pk is primary key of user model (not google id)
-    # returns queryset 
-    user_sessions = Session.objects.filter(user__id=user_pk).order_by("log_time")
+def get_sessions_from_user(user_google_id):
+    """Returns all sessions for a User given their google id sorted by time. 
+    Returns None if no sessions for user found. 
+    This would imply the user is not even in the database as all Users are guaranteed to have at least one session.
+
+    Args:
+        user_google_id (int): Google ID of user
+
+    Returns:
+        QuerySet: List containing each Session instance for user or None if no sessions found
+    """
+    user_sessions = Session.objects.filter(user__google_id=user_google_id).order_by("log_time")
     return user_sessions
